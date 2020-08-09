04ff69447ff16f1b9aeb6a743ead0a7377e239f6
Mike28e <ev3501@wayne.edu>
2020-08-09 18:40:05 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace general_cli
+{
+    class Program
+    {
+        static int Main(string[] args)
+            => CommandLineApplication.Execute<Program>(args);
+        
+        [Option(Description = "The subject")]
+        public string Subject {get;}
+
+        [Option(ShortName = "n")]
+        public int Count {get;}
+
+        private void OnExecute()
+        {
+            var subject = Subject ?? "world";
+            for(var i = 0; i<Count; i++)
+            {
+                Console.WriteLine($"Hello {Subject}!");
+            }
+        }
+
+    }
+}
---
+++ SendData.cs
@@ -0,0 +1,13 @@
+using McMaster.Extensions.CommandLineUtils;
+
+namespace general_cli.Commands
+{
+    public class SendData
+    {
+        private int OnExecute()
+        {
+            
+            return 0;
+        }
+    }
+}
